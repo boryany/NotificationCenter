ad47a81a69a5a07ee9a635b425924d36d3513259
engyordanov <eng.yordanov@gmail.com>
2025-01-25 10:49:53 +0200

    Add entities
    Add Notification and Client entities in Domain Layer

---
+++ NotificationCenter.Domain/Entities/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace NotificationCenter.Domain.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsIndividual { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+
+        public ICollection<Notification> Notifications { get; set; }  
+    }
+}
---
+++ NotificationCenter.Domain/Entities/Notification.cs
@@ -0,0 +1,14 @@
+﻿namespace NotificationCenter.Domain.Entities
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int EventId { get; set; }
+        public string Message { get; set; }
+        public bool IsRead { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Client Client { get; set; } 
+    }
+}
